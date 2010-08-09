d7cca95b42c4297deb5cde19f5befde0f34b0327
unknown <Justin@.(none)>
2010-08-08 20:49:13 -0700

    JT and I are discussing how to programmatically build complex fuselages. This simple m file creates a square and circular profile for later use while building some sort of interpolation function to interpolate different body profiles along the length of the body

---
+++ circle.m
@@ -0,0 +1,35 @@
+function [Z]=circle(center, NOP, radius)
+if (nargin <2),
+ error('Please see help for INPUT DATA.');
+elseif (nargin==3)
+    style='b-';
+end;
+THETA=linspace(0,2*pi,NOP);
+RHO=ones(1,NOP)*radius;
+[X,Y] = pol2cart(THETA,RHO);
+for i=1:NOP
+    if i < NOP/8
+        Xs(i) = 1;
+        Ys(i) = (i-1)/(NOP/8);
+    elseif i < 3*NOP/8
+        Ys(i) = 1;
+        Xs(i) = 1 - (i-(NOP/8))/(NOP/8);
+    elseif i < 5*NOP/8
+        Xs(i) = -1;
+        Ys(i) = 1-(i-3*NOP/8)/(NOP/8);
+    elseif i < 7*NOP/8
+        Ys(i) = -1;
+        Xs(i) = -1 + (i-5*NOP/8)/(NOP/8);
+    else 
+        Xs(i) = 1;
+        Ys(i) = -1 + (i-7*NOP/8)/(NOP/8);
+    end
+end
+X=X+center(1);
+X=X';
+Y=Y+center(2);
+Y=Y';
+Z=[X,Y, Xs', Ys'];
+plot(X,Y,Xs,Ys);
+[Xs ;Ys]
+axis square;
