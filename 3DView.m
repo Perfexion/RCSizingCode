94d4cd85ac7b9ef479d8da0ed39708d066b0b3fc
Justin <Justin@JustinDeeHoffman.com>
2010-08-17 16:11:08 -0700

    First commit of visualization module

--- 3DView.m
+++ 3DView.m
@@ -1,3 +1,12 @@
 function out = 3DView()
-
+    
+    z = circle([0,0], 100, .5);
+    profin(:,1:2) = z(:, 1:2);
+    profin(:,3) = 0;
+    profin(:,4:5) = z(:, 1:2);
+    profin(:,6) = 0;
+    profiles = profileinterp(profin,15);
+    
+    
+    
 end
